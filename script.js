8ee4ff7b18efe2d03b8780fb86df7630c96d8c49
elpk-oss <pelpepito755@gmail.com>
2025-06-22 18:34:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function mostrarSeccion(id) {
-  // Oculta todas las secciones
-  const secciones = document.querySelectorAll('.seccion');
-  secciones.forEach(sec => sec.classList.remove('activa'));
-
-  // Muestra la sección seleccionada
-  const activa = document.getElementById(id);
-  if (activa) activa.classList.add('activa');
-}
